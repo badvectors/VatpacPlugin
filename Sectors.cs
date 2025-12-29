afeabc43529fec808a6cd7c925fb3a827a5c4e68
AJ <46506175+badvectors@users.noreply.github.com>
2025-12-29 11:57:23 +1000

    Bug fix where only primary sectors would show if other atc online.

--- Sectors.cs
+++ Sectors.cs
@@ -1,19 +1,48 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using vatsys;
 
 namespace VatpacPlugin
 {
     public class Sectors
     {
+        public static void Init()
+        {
+            var primarySector = SectorsVolumes.Sectors.FirstOrDefault(x => x.Callsign == Network.Me.Callsign);
+
+            if (primarySector == null) return;
+
+            var sectors = new List<SectorsVolumes.Sector>
+            {
+                primarySector
+            };
+
+            foreach (var subsector in primarySector.SubSectors.ToList())
+            {
+                var onlineATC = Network.GetOnlineATCs.FirstOrDefault(x => x.Callsign == subsector.Callsign && x.IsRealATC);
+
+                if (onlineATC != null) continue;
+
+                sectors.Add(subsector);
+            }
+
+            MMI.SetControlledSectors(sectors);
+        }
+
         public static void CheckActive()
         {
             var primarySector = SectorsVolumes.Sectors.FirstOrDefault(x => x.Callsign == Network.Me.Callsign);
 
             if (primarySector == null) return;
 
+            var sectors = new List<SectorsVolumes.Sector>
+            {
+                primarySector
+            };
+
             var currentSectors = MMI.SectorsControlled.ToList();
 
-            foreach (var frequency in Audio.VSCSFrequencies)
+            foreach (var frequency in Audio.VSCSFrequencies.ToList())
             {
                 var frequencySector = SectorsVolumes.Sectors.FirstOrDefault(x => x.Callsign == frequency.Name);
 
@@ -23,28 +52,28 @@ namespace VatpacPlugin
 
                 if (currentlyControlled == null)
                 {
-                    // not currently controlled
-
                     if (!frequency.Transmit) continue;
 
                     currentSectors.Add(frequencySector);
 
-                    foreach (var subsector in frequencySector.SubSectors)
+                    foreach (var subsector in frequencySector.SubSectors.ToList())
                     {
+                        var onlineATC = Network.GetOnlineATCs.FirstOrDefault(x => x.Callsign == subsector.Callsign && x.IsRealATC);
+
+                        if (onlineATC != null) continue;
+
                         currentSectors.Add(subsector);
                     }
                 }
                 else
                 {
-                    // currently controlled
-
                     if (frequency.Transmit) continue;
 
                     if (primarySector == currentlyControlled) continue;
 
                     currentSectors.Remove(currentlyControlled);
 
-                    foreach (var subsector in currentlyControlled.SubSectors)
+                    foreach (var subsector in currentlyControlled.SubSectors.ToList())
                     {
                         currentSectors.Remove(subsector);
                     }
